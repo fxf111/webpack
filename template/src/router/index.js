1fb64c97cd6325582a9cdb44bd73aff685db4880
fxf111 <admin@js-home.cn>
2019-02-28 16:32:39 +0800

    路由懒加载

--- template/src/router/index.js
+++ template/src/router/index.js
@@ -1,12 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+Vue.use(Router)
 
 import config from '../../config'
 const baseRoot = config.rootPath.slice(0, config.rootPath.length-1); //根
 
-import Show from '@/components/views/Show/Show.vue'
+const Show = () =>
+    import (/* webpackChunkName: "show" */'cmp/views/Show/Show');
 
-Vue.use(Router)
 
 export default new Router({
   base: baseRoot,
